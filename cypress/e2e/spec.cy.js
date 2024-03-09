250c120bdbe0afbf15fc4175cb38368f916925ed
HeartBox12 <maximosity1@gmail.com>
2024-03-09 15:52:42 -0800

    added random file

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,5 +3,14 @@ describe('Charts', () => {
     cy.visit('localhost:8080')
 
     cy.contains("Line").click()
+    cy.contains("X label").type("Cats")
+    cy.contains("Y label").type("Dogs")
+
+    cy.findByText("X").type("1")
+    cy.findByText("Y").type("3")
+
+    cy.findByText("Generate chart").click()
+
+    cy.findByRole("img").should("exist")
   })
 })
