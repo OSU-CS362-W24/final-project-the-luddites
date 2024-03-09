742440378d0d1afa476407b42e93f2669d1e9d62
SageTafoya <101155383+SageTafoya@users.noreply.github.com>
2024-03-09 15:53:21 -0800

    Added some tests for the points, and jest test

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -1,1 +1,80 @@
 const sortPoints = require('./sortPoints');
+
+test('This tests that 2 potive points in decending order are rearanged to ascending order', function() {
+	expect(sortPoints(
+        [
+            { x: 8, y: 2 },
+            { x: 4, y: 4 }
+        ]
+    )).toStrictEqual(
+        [
+            { x: 4, y: 4 },
+            { x: 8, y: 2 }
+        ]
+    );
+
+});
+
+
+
+test('This tests that 2 points in ascending order will remain in ascending order', function() {
+	expect(sortPoints(
+        [
+            { x: 4, y: 4 },
+            { x: 8, y: 2 }
+        ]
+    )).toStrictEqual(
+        [
+            { x: 4, y: 4 },
+            { x: 8, y: 2 }
+        ]
+    );
+
+});
+
+test('This tests that 2 points in acending order that have float numbers reamain as float numbers', function() {
+	expect(sortPoints(
+        [
+            { x: 4.5, y: 4.9 },
+            { x: 8, y: 2 }
+        ]
+    )).toStrictEqual(
+        [
+            { x: 4.5, y: 4.9 },
+            { x: 8, y: 2 }
+        ]
+    );
+
+});
+
+
+test('This tests that 2 potive points in decending order with floats are rearanged to ascending order', function() {
+	expect(sortPoints(
+        [
+            { x: 8.45, y: 2.76 },
+            { x: 4, y: 4 }
+        ]
+    )).toStrictEqual(
+        [
+            { x: 4, y: 4 },
+            { x: 8.45, y: 2.76 }
+        ]
+    );
+
+});
+
+
+
+
+
+/*
+Edge Cases
+
+4.54, 3.64
+34343453, 45345345
+four, six
+-4, 3
+
+
+
+*/
