97934c4a3ea6f43f30612a889303d003636e61ad
HeartBox12 <maximosity1@gmail.com>
2024-03-09 15:22:32 -0800

    More changes included

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Charts', () => {
+  it('correctly generates a chart', () => {
     cy.visit('localhost:8080')
+
+    cy.contains("Line").click()
   })
 })
