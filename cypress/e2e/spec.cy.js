5409372ef9250c2af463675c37611f66fc499c7e
HeartBox12 <maximosity1@gmail.com>
2024-03-09 15:58:06 -0800

    Added the Data Retention Test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,4 +13,26 @@ describe('Charts', () => {
 
     cy.findByRole("img").should("exist")
   })
+  
+  it('keeps the data across pages', () => {
+    cy.visit('localhost:8080')
+
+    cy.contains("Line").click()
+    cy.contains("X label").type("Cats")
+    cy.contains("Y label").type("Dogs")
+
+    cy.findByText("X").type("1")
+    cy.findByText("Y").type("3")
+
+    cy.findByText("Scatter").click()
+
+    cy.findByDisplayValue("1").should("exist")
+    cy.findByDisplayValue("3").should("exist")
+
+    cy.findByText("Bar").click()
+
+    cy.findByDisplayValue("1").should("exist")
+    cy.findByDisplayValue("3").should("exist")
+
+  })
 })
