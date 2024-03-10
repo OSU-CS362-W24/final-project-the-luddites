958f1671af984e13047789be5bc39b959da67451
SageTafoya <101155383+SageTafoya@users.noreply.github.com>
2024-03-09 17:09:32 -0800

    added some more tests for float numbers

--- src/lib/sortPoints.test.js
+++ src/lib/sortPoints.test.js
@@ -63,10 +63,61 @@ test('This tests that 2 potive points in decending order with floats are rearang
 
 });
 
+test('This tests that 2 potive points in decending order with multible floats are rearanged to ascending order', function() {
+	expect(sortPoints(
+        [
+            { x: 8.45, y: 2.76 },
+            { x: 4.45, y: 4.54 }
+        ]
+    )).toStrictEqual(
+        [
+            { x: 4.45, y: 4.54 },
+            { x: 8.45, y: 2.76 }
+        ]
+    );
+
+});
 
 
+test('This tests that 2 potive points in acending order with multible floats are rearanged to ascending order', function() {
+	expect(sortPoints(
+        [
+            { x: 4.45, y: 4.54 },
+            { x: 8.45, y: 2.76 }
+        ]
+    )).toStrictEqual(
+        [
+            { x: 4.45, y: 4.54 },
+            { x: 8.45, y: 2.76 }
+        ]
+    );
 
+});
 
+test('This tests that 2 potive points in acending order with multible different sized floats are rearanged to ascending order', function() {
+	expect(sortPoints(
+        [
+            { x: 4.45, y: 4.54 },
+            { x: 8.4534, y: 2.7654 }
+        ]
+    )).toStrictEqual(
+        [
+            { x: 4.45, y: 4.54 },
+            { x: 8.4534, y: 2.7654 }
+        ]
+    );
+
+});
+
+test('This tests that 2 potive points in acending order with multible different sized floats are rearanged to ascending order', function() {
+	expect(sortPoints(
+        [
+            { x: e, y: 3 },
+            { x: 8.4534, y: 2.7654 }
+        ]
+    )).toBe(undefined);
+
+});
 /*
 Edge Cases
 
