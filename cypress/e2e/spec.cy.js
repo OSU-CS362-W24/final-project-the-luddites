a63f16cdf8e2a147874259108dd9b5e3f9adfcf8
HeartBox12 <maximosity1@gmail.com>
2024-03-09 16:54:11 -0800

    Created fourth test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -56,4 +56,26 @@ describe('Charts', () => {
 
     cy.findByRole("img").should("exist")
   })
+
+  it('can open a chart in the gallery', () => {
+    cy.visit('localhost:8080')
+
+    cy.contains("Line").click()
+    cy.contains("X label").type("Cats")
+    cy.contains("Y label").type("Dogs")
+
+    cy.findByText("X").type("1")
+    cy.findByText("Y").type("3")
+
+    cy.contains("Generate chart").click()
+
+    cy.contains("Save chart").click()
+
+    cy.contains("Gallery").click()
+
+    cy.findByRole("img").click()
+
+    cy.findByDisplayValue("1").should("exist")
+    cy.findByDisplayValue("3").should("exist")
+  })
 })
