e174ba9446e4de40c7f15334c9f901d9a0ce4be5
ArnoPorterOSU <porterar@oregonstate.edu>
2024-03-09 17:51:23 -0800

    attempting to write a SINGLE TEST and still haven't fixed it

--- src/lib/chartStorage.test.js
+++ src/lib/chartStorage.test.js
@@ -1,1 +1,41 @@
+/**
+ * @jest-environment jsdom
+ */
+
+// setup for testing
+const fs = require("fs")
+const domTesting = require('@testing-library/dom')
+const { assert } = require("console")
+const { hasUncaughtExceptionCaptureCallback } = require("process")
+require('@testing-library/jest-dom')
+const userEvent = require("@testing-library/user-event").default
+
+// helper function for initializing dom
+function initDomFromFiles(htmlPath, jsPath) {
+    const html = fs.readFileSync(htmlPath, 'utf8')
+    document.open()
+    document.write(html)
+    document.close()
+    jest.isolateModules(function() {
+        require(jsPath)
+    })
+}
+
 const chartStorage = require('./chartStorage');
+
+// saveChart tests -----------------------------
+
+// save BAR chart with no index specification
+test("saveChart with no index specification", function() {
+    // ARRANGE
+    // get stuff from dom via initializing
+    initDomFromFiles(`${__dirname}/../bar/bar.html`, `${__dirname}/chartStorage.js`);
+    // get chart
+    const myChart = document.getElementById("graph");
+    // no index specification
+    const myIndex = null;
+    // ACT
+    saveChart(myChart, myIndex);
+    // ASSERT
+    expect(localStorage.length()).toBe(1);
+})
