00340586d3a69745385c5444c6b15c2bbba06266
Jenna Cook <cookjen@oregonstate.edu>
2024-03-14 20:45:42 -0700

    Tests that values button doesn't impact data already entered

--- src/line/line.test.js
+++ src/line/line.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+
 const fs = require("fs")
 require("whatwg-fetch")            // Since I am not using ES6 modules or MSW, I have to add this require statement
 const domTesting = require('@testing-library/dom')
@@ -18,6 +19,10 @@ function initDomFromFiles(htmlPath, jsPath) {
     })
 }
 
+beforeEach(()=> {
+    window.localStorage.clear()
+})
+
 test("Add values button adds input field for X value", async function(){
     initDomFromFiles(`${__dirname}/line.html`,`${__dirname}/line.js`)
 
@@ -26,7 +31,7 @@ test("Add values button adds input field for X value", async function(){
     const user = userEvent.setup()
     await user.click(addValuesButton)  
     const xInputs = domTesting.queryAllByText(document, "X")
-    expect(xInputs[1]).not.toBe(undefined)   // Verifies a second X input field was created
+    expect(xInputs[1]).toBeDefined()    // Verifies a second X input field was created
 
 })
 
@@ -38,6 +43,35 @@ test("Add values button adds input field for Y value", async function(){
     const user = userEvent.setup()
     await user.click(addValuesButton)  
     const yInputs = domTesting.queryAllByText(document, "Y")
-    expect(yInputs[1]).not.toBe(undefined)   // Verifies a second Y input field was created
+    expect(yInputs[1]).toBeDefined()   // Verifies a second Y input field was created
 
 })
+
+test("Add values button doesn't impact any data already entered for X value", async function(){
+    initDomFromFiles(`${__dirname}/line.html`,`${__dirname}/line.js`)
+
+    const addValuesButton = domTesting.getByText(document, "+")
+    const xInput = domTesting.getByLabelText(document, "X")
+
+    const user = userEvent.setup()
+    await user.type(xInput, "1")
+    expect(xInput.value).toBe("1")        // Verifies that the data entered before clicking is 1
+    await user.click(addValuesButton)  
+    const xInputs = domTesting.queryAllByLabelText(document, "X")
+    expect(xInputs[0].value).toBe("1")   // Verifies that the data entered after clicking is still 1
+})
+
+test("Add values button doesn't impact any data already entered for Y value", async function(){
+    initDomFromFiles(`${__dirname}/line.html`,`${__dirname}/line.js`)
+
+    const addValuesButton = domTesting.getByText(document, "+")
+    const yInput = domTesting.getByLabelText(document, "Y")
+
+    const user = userEvent.setup()
+    await user.type(yInput, "3")
+    expect(yInput.value).toBe("3")        // Verifies that the data entered before clicking is 3
+    await user.click(addValuesButton)  
+    const yInputs = domTesting.queryAllByLabelText(document, "Y")
+    expect(yInputs[0].value).toBe("3")   // Verifies that the data entered after clicking is still 3
+})
+
