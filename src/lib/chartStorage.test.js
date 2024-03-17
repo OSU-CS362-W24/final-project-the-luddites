35cb9d561739589e18b2b7be3fd0850309f37924
ArnoPorterOSU <porterar@oregonstate.edu>
2024-03-17 15:34:05 -0700

    loadAllSavedCharts tests done

--- src/lib/chartStorage.test.js
+++ src/lib/chartStorage.test.js
@@ -12,10 +12,9 @@ beforeEach(function() {
 
 // saveChart tests -----------------------------
 
-// save chart with no index specification (only one chart saved)
 test("saveChart with no index specification (only one chart saved)", function() {
     // ARRANGE
-    // set up line chart
+    // set up chart
     var myChart = ({
         type: "line",
         data: [{x:1,y:3},{x:2,y:7},{x:3,y:15},{x:4,y:28},{x:5,y:50}],
@@ -33,10 +32,9 @@ test("saveChart with no index specification (only one chart saved)", function() 
     expect(chartStorage.loadSavedChart(0)).toMatchObject(myChart);
 })
 
-// save chart with no index specification (multiple charts saved)
 test("saveChart with no index specification (multiple charts saved)", function() {
     // ARRANGE
-    // set up line chart
+    // set up charts
     var myChart1 = ({
         type: "scatter",
         data: [{x:4,y:1},{x:5,y:11},{x:12,y:12},{x:15,y:12}],
@@ -64,10 +62,9 @@ test("saveChart with no index specification (multiple charts saved)", function()
     expect(chartStorage.loadSavedChart(1)).toMatchObject(myChart2);
 })
 
-// save chart with index specification (only one chart saved)
 test("saveChart with index specification (only one chart saved)", function() {
     // ARRANGE
-    // set up line chart
+    // set up chart
     var myChart = ({
         type: "bar",
         data: [{x:"Eagles",y:203},{x:"Ravens",y:197}],
@@ -85,10 +82,9 @@ test("saveChart with index specification (only one chart saved)", function() {
     expect(chartStorage.loadSavedChart(0)).toMatchObject(myChart);
 })
 
-// save chart with index specification (multiple charts saved in order)
 test("saveChart with index specification (multiple charts saved in order)", function() {
     // ARRANGE
-    // set up line chart
+    // set up charts
     var myChart1 = ({
         type: "line",
         data: [{x:1,y:3},{x:2,y:7},{x:3,y:15},{x:4,y:28},{x:5,y:50}],
@@ -116,10 +112,9 @@ test("saveChart with index specification (multiple charts saved in order)", func
     expect(chartStorage.loadSavedChart(1)).toMatchObject(myChart2);
 })
 
-// save chart with index specification (overrided a chart)
 test("saveChart with index specification (overrided a chart)", function() {
     // ARRANGE
-    // set up line chart
+    // set up charts
     var myChart1 = ({
         type: "line",
         data: [{x:1,y:3},{x:2,y:7},{x:3,y:15},{x:4,y:28},{x:5,y:50}],
@@ -157,3 +152,53 @@ test("saveChart with index specification (overrided a chart)", function() {
 })
 
 // loadAllSavedCharts tests -----------------------------
+
+test("loadAllSavedCharts loads a single saved chart", function() {
+    // ARRANGE
+    // set up chart
+    var myChart = ({
+        type: "line",
+        data: [{x:1,y:3},{x:2,y:7},{x:3,y:15},{x:4,y:28},{x:5,y:50}],
+        xLabel: "Cats",
+        yLabel: "Dogs",
+        title: "Cats vs. Dogs",
+        color: "#ffa500"
+    })
+
+    // ACT
+    // save chart
+    chartStorage.saveChart(myChart, null);
+
+    // ASSERT
+    expect(chartStorage.loadAllSavedCharts()[0]).toMatchObject(myChart);
+})
+
+test("loadAllSavedCharts loads multiple saved charts", function() {
+    // ARRANGE
+    // set up charts
+    var myChart1 = ({
+        type: "scatter",
+        data: [{x:4,y:1},{x:5,y:11},{x:12,y:12},{x:15,y:12}],
+        xLabel: "Mice",
+        yLabel: "Rats",
+        title: "Mice vs. Rats",
+        color: "#ff0000"
+    })
+    var myChart2 = ({
+        type: "bar",
+        data: [{x:"Owls",y:32},{x:"Crows",y:102}],
+        xLabel: "Bird types",
+        yLabel: "Population",
+        title: "Bird populations by type",
+        color: "#0000ff"
+    })
+
+    // ACT
+    // save chart
+    chartStorage.saveChart(myChart1, null);
+    chartStorage.saveChart(myChart2, null);
+
+    // ASSERT
+    expect(chartStorage.loadAllSavedCharts()[0]).toMatchObject(myChart1);
+    expect(chartStorage.loadAllSavedCharts()[1]).toMatchObject(myChart2);
+})
