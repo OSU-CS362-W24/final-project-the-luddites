9ee3e4865fd4b32fe195d4cc676420cac2eedf19
SageTafoya <101155383+SageTafoya@users.noreply.github.com>
2024-03-20 17:09:18 -0700

    added another test for an empty title

--- src/lib/generateChartImg.test.js
+++ src/lib/generateChartImg.test.js
@@ -121,3 +121,64 @@ test("Test generate chart with all inputs valid and rgb css color input", async 
     // console.log(myChart);
     // expect(chart).toMatchObject(chart);
 })
+
+test("Test that color set as an empty string will be seen as valid", async () => {
+
+    const data = [
+        { x: 1, y: 5 },
+        { x: 2, y: 7 },
+        { x: 3, y: 9 },
+        { x: 4, y: 6 },
+        { x: 5, y: 8 }
+    ];
+
+    const type = "line";
+    const xLabel = "X Axis";
+    const yLabel = "Y Axis";
+    const title = "Example Line Chart";
+    const color = ""; 
+
+    // generateChartImg(type, data, xLabel, yLabel, title, color).then()
+    const returndata = await generateChartImg(type, data, xLabel, yLabel, title, color);
+
+
+    console.log(returndata);
+
+    expect(returndata).toMatch(/blob:nodedata/);
+    // chartStorage.saveChart(myChart, null);
+
+    // ASSERT
+    // console.log(myChart);
+    // expect(chart).toMatchObject(chart);
+})
+
+
+test("Test that has title set as an empty string will be seen as valid", async () => {
+
+    const data = [
+        { x: 1, y: 5 },
+        { x: 2, y: 7 },
+        { x: 3, y: 9 },
+        { x: 4, y: 6 },
+        { x: 5, y: 8 }
+    ];
+
+    const type = "line";
+    const xLabel = "X Axis";
+    const yLabel = "Y Axis";
+    const title = "";
+    const color = "#FF5733"; 
+
+    // generateChartImg(type, data, xLabel, yLabel, title, color).then()
+    const returndata = await generateChartImg(type, data, xLabel, yLabel, title, color);
+
+
+    console.log(returndata);
+
+    expect(returndata).toMatch(/blob:nodedata/);
+    // chartStorage.saveChart(myChart, null);
+
+    // ASSERT
+    // console.log(myChart);
+    // expect(chart).toMatchObject(chart);
+})
